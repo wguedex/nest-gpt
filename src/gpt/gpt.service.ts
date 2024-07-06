afd3d91a8629fdba353d05283d640654d27cced0
Wilmer Guedez <w.guedex@gmail.com>
2024-07-06 17:27:16 -0500

    nest-gpt: creacion de primer prompt

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 
 @Injectable()
 export class GptService {
- 
+
+    private openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    })
+    
     async orthographycheck(orthographyDto: OrthographyDto){
+
+        console.log(process.env.OPENAI_API_KEY)
  
-        return await orthographyCheckUseCase({
-            prompt: orthographyDto.prompt
+        return await orthographyCheckUseCase(this.openai, {
+            prompt: orthographyDto.prompt, 
         });
 
     }
-
+    
 }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,15 +1,37 @@
-
+import OpenAI from "openai";
 
 interface Options {
-    prompt: string;
+  prompt: string;
 }
 
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options
+) => {
+  const { prompt } = options;
 
-export const orthographyCheckUseCase = async (options: Options) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          `
+          Te serán provistos textos con posibles errores ortográficos y gramaticales. 
+          Debes responder en formato JSON. Tu tarea es corregirlos y retornar información solucionada. 
+          También debes dar un porcentaje de acierto para el usuario. 
+          Si no hay errores, debes retornar un mensaje de felicitaciones.
+          `
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+    temperature:0.3, 
+    max_tokens: 150
+  });
 
-    const { prompt } = options;
-
-    return {
-        prompt: prompt
-    }
-}
+  console.log(completion);
+  return completion.choices[0];
+};
