2bf97b97b416207f71ce8fb1a77f9305db4634ff
Wilmer Guedez <w.guedex@gmail.com>
2024-07-06 17:40:18 -0500

    nest-gpt: retornar un json con la informacion

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -12,20 +12,32 @@ export const orthographyCheckUseCase = async (
 
   const completion = await openai.chat.completions.create({
     messages: [
-      {
-        role: "system",
-        content:
-          `
-          Te serán provistos textos con posibles errores ortográficos y gramaticales. 
-          Debes responder en formato JSON. Tu tarea es corregirlos y retornar información solucionada. 
-          También debes dar un porcentaje de acierto para el usuario. 
-          Si no hay errores, debes retornar un mensaje de felicitaciones.
-          `
-      },
-      {
-        role: "user",
-        content: prompt,
-      },
+        { 
+            role: "system", 
+            content: `
+            Te serán proveídos textos en español con posibles errores ortográficos y gramaticales,
+            Las palabras usadas deben de existir en el diccionario de la Real Academia Española,
+            Debes de responder en formato JSON, 
+            tu tarea es corregirlos y retornar información soluciones, 
+            también debes de dar un porcentaje de acierto por el usuario,
+            
+    
+            Si no hay errores, debes de retornar un mensaje de felicitaciones.
+    
+            Ejemplo de salida:
+            {
+              userScore: number,
+              errors: string[], // ['error -> solución']
+              message: string, //  Usa emojis y texto para felicitar al usuario
+            }
+            
+            
+            `
+          },
+          {
+            role: 'user',
+            content: prompt,
+          }
     ],
     model: "gpt-3.5-turbo-1106",
     temperature:0.3, 
