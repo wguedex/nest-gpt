ad02ece2873509acd624b6d7c3c031d31e12feb3
Wilmer Guedez <w.guedex@gmail.com>
2024-07-06 00:38:00 -0500

    OpenAI - configuracion de cuenta y token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GptModule } from './gpt/gpt.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GptModule],
+  imports: [ConfigModule.forRoot(), 
+             GptModule],
   controllers: [],
   providers: [],
 })
