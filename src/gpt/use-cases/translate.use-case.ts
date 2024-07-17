fdfb7af2ac487a49b35346130cd05982485eefbd
Wilmer Guedez <w.guedex@gmail.com>
2024-07-16 22:32:31 -0500

    translate text - use case

--- src/gpt/use-cases/translate.use-case.ts
+++ src/gpt/use-cases/translate.use-case.ts
@@ -1,27 +1,27 @@
 import OpenAI from "openai";
 
 interface Options {
-  prompt: string;
-  lang : string;
-}
-
-export const translateUseCase = async (openai: OpenAI, { prompt, lang }: Options) => {
-
-  const response= await openai.chat.completions.create({
-    model: 'gpt-4',
-    messages: [
-      {
-        role: 'system',
-        content: `Traduce el siguiente texto al idioma ${lang}:${ prompt }`
-      },
-      {
-        role: 'user',
-        content: prompt
-      }
-    ],
-    temperature: 0.8,
-    max_tokens: 500
-  })
-
-  return response.choices[0].message;
-}
+    prompt: string;
+    lang: string;
+  }
+  
+  
+  
+  export const translateUseCase = async (openai: OpenAI, { prompt, lang }: Options) => {
+  
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4',
+      messages: [
+        {
+          role: 'system',
+          content: `Traduce el siguiente texto al idioma ${lang}:${ prompt }`
+        },
+      ],
+      temperature: 0.2,
+      // max_tokens: 500
+    })
+  
+  
+  
+    return { message: response.choices[0].message.content }
+  }
